86a12e7f7b547884cbe1f673fe80bf3375b5e8ba
Souza <sanfisher@msn.com>
2025-07-24 16:17:44 -0300

    Criado o diretório repository com suas classes AutorRepository e LivroRepository

--- src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
+++ src/main/java/io/github/cursodsousa/libraryapi/LibraryapiApplication.java
@@ -1,13 +1,17 @@
 package io.github.cursodsousa.libraryapi;
 
+import io.github.cursodsousa.libraryapi.model.Autor;
+import io.github.cursodsousa.libraryapi.repository.AutorRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 public class LibraryapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryapiApplication.class, args);
-	}
 
+	}
 }
